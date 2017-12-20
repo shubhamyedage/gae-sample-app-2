e6bd1e83eb0d38746f7cfbe3e695f3525e399a6b
shubham.yedage <shubham.yedage@synerzip.com>
2017-12-20 16:19:29 +0530

    updated ouput string

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import webapp2
 
 class HomePage(webapp2.RequestHandler):
     def get(self):
-        self.response.write("Hello world!")
+        self.response.write("Hello world!!")
 
 app = webapp2.WSGIApplication([
     ('/', HomePage)
